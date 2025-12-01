5793d020dc576a0a34f1b493e460cabc9a8b7621
Tao <96sutao@gmail.com>
2025-11-30 20:35:28 -0500

    update: created the service for users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import {AppController} from "../app.controller";
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
     controllers: [UsersController],
-    providers: []
+    providers: [UsersService]
 })
 export class UsersModule {}
