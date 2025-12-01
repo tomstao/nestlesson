789a3a13545c27f1a409fe306462c84c466c8a7e
Tao <96sutao@gmail.com>
2025-11-30 21:28:44 -0500

    feat: created controller for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+     */
+
+    @Get()
+    findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | "ENGINEER" ) {
+        return []
+    }
+
+    @Get('interns')
+    findAllInterns() {
+        return []
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return {id}
+    }
+
+    @Post()
+    create(@Body() user: {}) {
+        return user
+    }
+
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return {id, ...userUpdate}
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return {id}
+    }
+
+}
